939748cafdbd458999506c7a0d616c38cf22d303
DDoS <9475098@gmail.com>
2023-08-13 22:52:19 +0300

    added D_z_iter_gen2

--- D_z_iter_gen/gen.py
+++ D_z_iter_gen/gen.py
@@ -1,3 +1,4 @@
+import types
 nested_list = [
 	['a', 'b', 'c'],
 	['d', 'e', 'f'],
@@ -18,3 +19,25 @@ if __name__ == '__main__':
         print(item)
 
 
+def test_2():
+
+    list_of_lists_1 = [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f', 'h', False],
+        [1, 2, None]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            flat_generator(list_of_lists_1),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+    ):
+
+        assert flat_iterator_item == check_item
+
+    assert list(flat_generator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+
+    assert isinstance(flat_generator(list_of_lists_1), types.GeneratorType)
+
+
+if __name__ == '__main__':
+    test_2()
